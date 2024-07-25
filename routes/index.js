c64354c89f4ac3c88c62674c878bdbbda0a764dc
Annisa Nur Pratiwi <129244088+annisanp373@users.noreply.github.com>
2024-07-26 02:57:20 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Succesfully');
+  res.send('Congratulations');
 });
 
 module.exports = router;
